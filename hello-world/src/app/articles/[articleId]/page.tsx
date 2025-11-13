b133bbabb9ed1c919bd100f86bfae75896413c83
seetharampullela <seetharam.pullela@gmail.com>
2025-11-13 13:55:10 +0530

    changes on usage of params/searchParams on article route

---
+++ hello-world/src/app/articles/[articleId]/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+import { use } from "react";
+
+export default function NewsArticle({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ articleId: string }>;
+  searchParams: Promise<{ lang?: "en" | "es" | "fr" }>;
+}) {
+  const { articleId } = use(params);
+  const { lang = "es" } = use(searchParams);
+
+  return (
+    <div>
+      <h1>News Article {articleId}</h1>
+      <p>Reading in Language {lang}</p>
+      <div>
+        <Link href={`/articles/${articleId}?lang=en`}>English</Link>
+        <Link href={`/articles/${articleId}?lang=fr`}>French</Link>
+        <Link href={`/articles/${articleId}?lang=es`}>Spanish</Link>
+      </div>
+    </div>
+  );
+}
