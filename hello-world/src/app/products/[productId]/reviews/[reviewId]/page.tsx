073331c7e2cca3753f5b8b31688a38900d089448
seetharampullela <seetharam.pullela@gmail.com>
2025-11-13 16:07:10 +0530

    changes for programmatic navigation using redirect, router

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,18 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
-export default async function ProductReviewDetails({ params }: { params: Promise<{  reviewId: string, productId: string }> }) {
-    const { productId, reviewId } = await params;
-    if(parseInt(reviewId) > 1000){
-        notFound()
-    }
-    return (
-        <h1>Review {reviewId} for the Product {productId}</h1>
-    );
+export default async function ProductReviewDetails({
+  params,
+}: {
+  params: Promise<{ reviewId: string; productId: string }>;
+}) {
+  const { productId, reviewId } = await params;
+  if (parseInt(reviewId) > 1000) {
+    // notFound()
+    redirect(`/products/${productId}`);
+  }
+  return (
+    <h1>
+      Review {reviewId} for the Product {productId}
+    </h1>
+  );
 }
