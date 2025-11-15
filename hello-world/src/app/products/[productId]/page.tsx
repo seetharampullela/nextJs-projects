c0b0e1b8890ad6dfafa983ed06049e0dd245af25
seetharampullela <seetharam.pullela@gmail.com>
2025-11-15 13:55:30 +0530

    changes for the error page in product with random integer

--- hello-world/src/app/products/[productId]/page.tsx
+++ hello-world/src/app/products/[productId]/page.tsx
@@ -1,6 +1,26 @@
-export default async function ProductDetails({params}: {params: Promise<{productId: string}>}) {
-    const productId = (await params).productId;
-  return (
-          <h1>Details about the Product {productId}</h1>
-  );
+import { Metadata } from "next";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}): Promise<Metadata> => {
+  const productId = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(` ${productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title} Details`,
+  };
+};
+
+export default async function ProductDetails({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const productId = (await params).productId;
+  return <h1>Details about the Product {productId}</h1>;
 }
--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,19 @@
 import { notFound, redirect } from "next/navigation";
 
+function getrandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
 export default async function ProductReviewDetails({
   params,
 }: {
   params: Promise<{ reviewId: string; productId: string }>;
 }) {
+  const random = getrandomInt(2);
+  if (random == 1) {
+    throw new Error("Failed loading review details");
+  }
+
   const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
     // notFound()
