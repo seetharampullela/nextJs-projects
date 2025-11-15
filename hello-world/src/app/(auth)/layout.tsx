f6a6904bc178d0b623998eefcc960311308f1d83
seetharampullela <seetharam.pullela@gmail.com>
2025-11-15 13:25:50 +0530

    changes for layout file holding state, it doesnot re-render the entire UI only the modified parts

--- hello-world/src/app/(auth)/layout.tsx
+++ hello-world/src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function AuthLayout({
   children,
@@ -14,34 +15,45 @@ export default function AuthLayout({
   ];
 
   const pathname = usePathname();
-
+  const [input, setInput] = React.useState("");
   return (
-    <html lang="en">
-      <body>
-        <header style={{ padding: "1rem" }}>
-          <nav>
-            <ul>
-              {routeMap.map((route) => {
-                const isActive = route.path === pathname;
-                console.log("route", isActive);
-                return (
-                  <li key={route.path}>
-                    <Link
-                      href={route.path}
-                      className={
-                        isActive ? "font-bold mt-8" : "color-grey-500 mt-8"
-                      }
-                    >
-                      {route.name}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </header>
-        {children}
-      </body>
-    </html>
+    <div>
+      <div>
+        <input
+          type="text"
+          value={input}
+          style={{
+            border: "1px solid white",
+            margin: "1rem",
+            padding: "0.5rem",
+          }}
+          placeholder="Search..."
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <header style={{ padding: "1rem" }}>
+        <nav>
+          <ul>
+            {routeMap.map((route) => {
+              const isActive = route.path === pathname;
+              console.log("route", isActive);
+              return (
+                <li key={route.path}>
+                  <Link
+                    href={route.path}
+                    className={
+                      isActive ? "font-bold mt-8" : "color-grey-500 mt-8"
+                    }
+                  >
+                    {route.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {children}
+    </div>
   );
 }
