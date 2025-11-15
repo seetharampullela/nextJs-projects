c5bd186f9f953b71a1089d65af2fa8e4d9b76745
seetharampullela <seetharam.pullela@gmail.com>
2025-11-15 14:13:01 +0530

    changes on programmatic refresh in error page with try again button

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,27 @@
 "use client";
 
-export default function ErroBoundary({ error }: { error: Error }) {
-  return <h1>{error.message}</h1>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function ErroBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  /* Similar to Page Reload. Programmatic refresh */
+  const router = useRouter();
+  const reloadPage = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <h1>{error.message}</h1>
+      <button onClick={() => reloadPage()}>Try again</button>
+    </div>
+  );
 }
